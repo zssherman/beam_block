7f9a82679c42a7e511c0ca90433208db374c90e6
Zachary Sherman <zsherman@anl.gov>
2017-03-02 16:36:30 -0600

    ADD: Adding __init__.py __init__.py is being added and currently imports functions from bb_from_radar.

---
+++ beam_block/core/__init__.py
@@ -0,0 +1,20 @@
+"""
+=============================
+Core (:mod:`beam_block.core`)
+=============================
+
+.. currentmodule:: beam_block.core
+
+Core Beam Block function for retrieving beam block fractions and flags.
+
+Core Functions
+==============
+
+.. autosummary::
+    :toctree: generated/
+
+    Beam Block from Radar
+
+"""
+
+from .bb_from_radar import *
