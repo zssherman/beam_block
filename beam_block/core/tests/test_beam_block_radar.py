cd139d125aaf9325fe40b4c3363bc0ab8f85b386
Zachary Sherman <zsherman@anl.gov>
2017-04-04 14:48:04 -0500

    DEV: Adding tests for the beam_block_radar.py module. This file creates unit tests for the beam_block function and beam_block_flags function found in beam_block_radar.py.

---
+++ beam_block/core/tests/test_beam_block_radar.py
@@ -0,0 +1,43 @@
+""" Unit Tests for Beam Block's core/beam_block_radar.py module. """
+
+import pyart
+import numpy
+from numpy.testing import assert_almost_equal
+
+import beam_block
+
+
+def test_beam_block():
+    radar = pyart.io.read(beam_block.testing.SAMPLE_RADAR_NC_FILE)
+    tif_file = beam_block.testing.SAMPLE_TIF_FILE
+    beam_width = 1.0
+
+    radar_bb_data = pyart.io.read(
+        beam_block.testing.SAMPLE_RADAR_BLOCK_DATA_FILE)
+    pbb_existing = radar_bb_data.fields['partial_beam_block']['data']
+    cbb_existing = radar_bb_data.fields['cumulative_beam_block']['data']
+
+    pbb_all, cbb_all = beam_block.core.beam_block(
+        radar, tif_file, beam_width)
+
+    assert_almost_equal(pbb_all, pbb_existing, 3)
+    assert_almost_equal(cbb_all, cbb_existing, 3)
+
+def test_beam_block_flags():
+    radar_bb_data = pyart.io.read(
+        beam_block.testing.SAMPLE_RADAR_BLOCK_DATA_FILE)
+    pbb_existing = radar_bb_data.fields['partial_beam_block']['data']
+    cbb_existing = radar_bb_data.fields['cumulative_beam_block']['data']
+
+    pbb_flags_existing = radar_bb_data.fields[
+        'partial_beam_block_flag']['data']
+    cbb_flags_existing = radar_bb_data.fields[
+        'cumulative_beam_block_flag']['data']
+    pbb_threshold = 0.01
+    cbb_threshold = 0.01
+
+    pbb_flags, cbb_flags = beam_block.core.beam_block_flags(
+        pbb_existing, cbb_existing, pbb_threshold, cbb_threshold)
+
+    assert_almost_equal(pbb_flags, pbb_flags_existing, 3)
+    assert_almost_equal(cbb_flags, cbb_flags_existing, 3)
