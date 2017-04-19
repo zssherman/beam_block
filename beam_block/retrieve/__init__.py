6179d86471c0d1d01ca8d5dec8972c65b35c84eb
Zachary Sherman <zsherman@anl.gov>
2017-04-19 11:10:52 -0500

    MAINT: Adding imports for new beam block locator functions.

--- beam_block/retrieve/__init__.py
+++ beam_block/retrieve/__init__.py
@@ -6,7 +6,8 @@ Core (:mod:`beam_block.retrieve`)
 .. currentmodule:: beam_block.retrieve
 
 Retrieve elevation function for retrieving lowest elevations needed
-for a gate to achieve less than 0.01 CBB fraction.
+for a gate to achieve less than 0.01 CBB fraction. Functions that return
+ray gate values where or where not blockage is occurring.
 
 Core Functions
 ==============
@@ -15,9 +16,15 @@ Core Functions
     :toctree: generated/
 
     lowest_elevation_no_blockage
+    locate_block_pbb
+    locate_block_cbb
+    locate_no_block_pbb
+    locate_no_block_cbb
 
 """
 
 from .low_el_no_block import lowest_elevation_no_blockage
+from .locate_block import locate_block_pbb, locate_block_cbb
+from .locate_block import locate_no_block_pbb, locate_no_block_cbb
 
 __all__ = [s for s in dir() if not s.startswith('_')]
