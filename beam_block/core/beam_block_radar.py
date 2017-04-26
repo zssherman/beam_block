1e3a41978525c5991c9879a02564ef975a588f19
Zachary Sherman <zsherman@anl.gov>
2017-04-26 16:06:17 -0500

    MAINT: Changed docstring, reflect wradlib 0.10 release. Changed pyart docstring to beam_block at the top of the module, beam_block most likely will stay its own package. Changed raster reader to reflect wradlib 0.10 update.

--- beam_block/core/beam_block_radar.py
+++ beam_block/core/beam_block_radar.py
@@ -1,9 +1,6 @@
-# Note docstring below is as if beam_block will be added
-# to Py-ART in the future.
-
 """
-pyart.retrieve.beam_block_radar
-===============================
+beam_block.core.beam_block_radar
+================================
 
 Calculates partial beam block(PBB) and cumulative beam block(CBB)
 by using wradlib's beamblock and geotiff functions. PBB and CBB
@@ -76,7 +73,6 @@ def beam_block(radar, tif_file,
     DOI: http://doi.org/10.5334/jors.119
 
     """
-
     # Emptying the radar fields.
     radar.fields.clear()
 
@@ -84,8 +80,8 @@ def beam_block(radar, tif_file,
     # converted into polar values.
     rasterfile = tif_file
     data_raster = wrl.io.open_raster(rasterfile)
-    proj_raster = wrl.georef.wkt_to_osr(data_raster.GetProjection())
-    rastercoords, rastervalues = wrl.io.read_raster_data(rasterfile)
+    rastervalues, rastercoords, proj = wrl.georef.extract_raster_dataset(
+        data_raster, nodata=None)
     sitecoords = (np.float(radar.longitude['data']),
                   np.float(radar.latitude['data']),
                   np.float(radar.altitude['data']))
@@ -97,17 +93,17 @@ def beam_block(radar, tif_file,
     # Cycling through all sweeps in the radar object.
     for i in range(len(radar.sweep_start_ray_index['data'])):
         index_start = radar.sweep_start_ray_index['data'][i]
-        index_end = radar.sweep_end_ray_index['data'][i]
+        index_end = radar.sweep_end_ray_index['data'][i] + 1
 
-        elevs = radar.elevation['data'][index_start:index_end + 1]
-        azimuths = radar.azimuth['data'][index_start:index_end + 1]
+        elevs = radar.elevation['data'][index_start:index_end]
+        azimuths = radar.azimuth['data'][index_start:index_end]
         rg, azg = np.meshgrid(_range, azimuths)
         rg, eleg = np.meshgrid(_range, elevs)
         lon, lat, alt = wrl.georef.polar2lonlatalt_n(
             rg, azg, eleg, sitecoords)
 
         x_pol, y_pol = wrl.georef.reproject(
-            lon, lat, projection_target=proj_raster)
+            lon, lat, projection_target=proj)
         polcoords = np.dstack((x_pol, y_pol))
         rlimits = (x_pol.min(), y_pol.min(), x_pol.max(), y_pol.max())
         ind = wrl.util.find_bbox_indices(rastercoords, rlimits)
@@ -131,17 +127,59 @@ def beam_block(radar, tif_file,
     cbb_all = np.ma.concatenate(cbb_arrays)
     return pbb_all, cbb_all
 
-def beam_block_flags(pbb_all, cbb_all, pbb_threshold=0.01,
-                     cbb_threshold=0.01):
-    """ Takes PBB and CBB arrays created from the
-    beam_block function and user chosen thresholds
-    to create and array of 1s and 0s, 1 is a flagged gate
-    where the fraction value is past the threshold. """
+
+def beam_block_flags(pbb_all, cbb_all, no_block_thresh=0.01,
+                     complete_block_thresh=0.95):
+    """
+    Takes PBB and CBB arrays created from the beam_block function
+    and creates beam block flags.
+
+    Parameters
+    ----------
+    pbb_all : array
+        Array of partial beam blockage values created from the beam_block
+        function.
+    cbb_all : array
+        Array of cumulative beam blockage values created from the beam_block
+        function.
+
+    Other Parameters
+    ----------------
+    no_block_thresh : float
+        Value for the cutoff for no blockage flag value of 0. Anything below
+        the no_block_thres is considered not blocked. Default value is 0.01.
+    complete_block_thresh : float
+        Value for the cutoff for complete blockage flag value of 2. Anything
+        above the complete_block_thres is considered blocked. Default value
+        is 0.95.
+
+    Returns
+    -------
+    pbb_flags : array
+        Array of integer values depicting no, partial, and complete beam
+        blockage based on the partial beam blockage data. This array can then
+        be used to create a partial beam block flags field.
+    cbb_flags : array
+        Array of integer values depicting no, partial, and complete beam
+        blockage based on the cumulative beam blockage data. This array can
+        then be used to create a cumulative beam block flags field.
+
+    Note
+    ----
+    The no_block_thresh and complete_block_thresh are also used to created
+    the partial blockage flag value of 1, by having anything between the
+    no_block_thresh and complete_block_thresh = 1.
+
+    """
     pbb_flags = np.empty_like(pbb_all)
-    pbb_flags[pbb_all > pbb_threshold] = 1
-    pbb_flags[pbb_all < pbb_threshold] = 0
+    pbb_flags[pbb_all > complete_block_thresh] = 2
+    pbb_flags[
+        (pbb_all > no_block_thresh) & (pbb_all < complete_block_thresh)] = 1
+    pbb_flags[pbb_all < no_block_thresh] = 0
 
     cbb_flags = np.empty_like(cbb_all)
-    cbb_flags[cbb_all > cbb_threshold] = 1
-    cbb_flags[cbb_all < cbb_threshold] = 0
+    cbb_flags[cbb_all > complete_block_thresh] = 2
+    cbb_flags[
+        (cbb_all > no_block_thresh) & (cbb_all < complete_block_thresh)] = 1
+    cbb_flags[cbb_all < no_block_thresh] = 0
     return pbb_flags, cbb_flags
