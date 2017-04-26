a844f3b264c81c8591be4ce76bde0055fb5fb775
Zachary Sherman <zsherman@anl.gov>
2017-04-26 16:00:13 -0500

    TST: Removed test for json_flag function that no longer exist.

--- beam_block/core/tests/test_beam_block_json.py
+++ beam_block/core/tests/test_beam_block_json.py
@@ -17,7 +17,7 @@ def test_json_beam_block():
     beam_width = 1.0
 
     radar_bb_data = pyart.io.read(
-        beam_block.testing.SAMPLE_RADAR_JSON_TO_NC_FILE)
+        beam_block.testing.SAMPLE_RADAR_BLOCK_DATA_FILE)
     pbb_existing = radar_bb_data.fields['partial_beam_block']['data']
     cbb_existing = radar_bb_data.fields['cumulative_beam_block']['data']
 
@@ -26,23 +26,3 @@ def test_json_beam_block():
 
     assert_almost_equal(pbb_all, pbb_existing, 3)
     assert_almost_equal(cbb_all, cbb_existing, 3)
-
-def test_json_beam_block_flags():
-    """ Unit test for the beam_block_json.json_beam_block_flags function. """
-    radar_bb_data = pyart.io.read(
-        beam_block.testing.SAMPLE_RADAR_JSON_TO_NC_FILE)
-    pbb_existing = radar_bb_data.fields['partial_beam_block']['data']
-    cbb_existing = radar_bb_data.fields['cumulative_beam_block']['data']
-
-    pbb_flags_existing = radar_bb_data.fields[
-        'partial_beam_block_flag']['data']
-    cbb_flags_existing = radar_bb_data.fields[
-        'cumulative_beam_block_flag']['data']
-    pbb_threshold = 0.01
-    cbb_threshold = 0.01
-
-    pbb_flags, cbb_flags = beam_block.core.json_beam_block_flags(
-        pbb_existing, cbb_existing, pbb_threshold, cbb_threshold)
-
-    assert_almost_equal(pbb_flags, pbb_flags_existing, 3)
-    assert_almost_equal(cbb_flags, cbb_flags_existing, 3)
